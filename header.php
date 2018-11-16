bf0d9abf6ebf967e38be1fb1786d5586dbffab2c
Eterniitys <johan.guerrero394@gmail.com>
2018-11-16 17:48:03 +0000

    Menu : add link icons

--- header.php
+++ header.php
@@ -29,21 +29,21 @@
 		<h2>Menu</h2>
 		<?php endif ;?>
 		<ul>
-			<li><a href="index.php">Home</a></li>
-			<li><a href="vue.php?artistes">Tous les artistes</a></li>
-			<li><a href="vue.php?concerts">Tous les concerts</a></li>
-			<li><a href="#">Mon Panier</a></li>
+			<li><a href="index.php" class="icon fa-home"> Home</a></li>
+			<li><a href="vue.php?artistes" class="icon fa-user"> Tous les artistes</a></li>
+			<li><a href="vue.php?concerts" class="icon fa-users"> Tous les concerts</a></li>
+			<li><a href="#" class="icon fa-shopping-cart"> Mon Panier</a></li>
 			<?php if(isset($_SESSION[admin]) && $_SESSION[admin] == true):?>
 			<!--Liens Administrateurs -->
-			<li><a href="gestionSite.php">Administration du Site</a></li>
-			<li><a href="elements.php">Elements</a></li>
+			<li><a href="gestionSite.php" class="icon fa-cog"> Administration du Site</a></li>
+			<li><a href="elements.php" class="icon fa-lock"> Elements</a></li>
 			<?php endif ;?>
 			<!--Liens utilisateur connecté -->
 			<?php if($_SESSION[id_utilisateur] >= 0):?>
-				<li><a href="sessionFin.php">Deconnexion</a></li>
+				<li><a href="sessionFin.php" class="icon fa-toggle-on"> Deconnexion</a></li>
 			<?php else :?>
 			<!--Liens hors connexion -->
-				<li><a href="accesCompte.php">Connexion</a></li>
+				<li><a href="accesCompte.php" class="icon fa-toggle-off"> Connexion</a></li>
 			<?php endif ;?>
 			<!--li><a href="vue-artiste.php">Tempus etiam</a></li>
 			<li><a href="vue-artiste.php">Consequat dolor</a></li-->
